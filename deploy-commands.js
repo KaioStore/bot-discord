c9769e19fdaf0d221856b43efc7710b30f5073ac
KaioStore <micaeljonata8@gmail.com>
2026-03-27 19:44:02 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -63,9 +63,8 @@ const commands = [
 
   // 🔥 EMBED
   new SlashCommandBuilder()
-    .setName('embed')
-    .setDescription('Criar embed profissional')
-
+  .setName('embed')
+  .setDescription('Abrir painel embed')
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
